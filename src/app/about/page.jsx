8469d1ca63d465da011b5b8ecbb5263d8294a605
Ssismy <gshasha124@gmail.com>
2024-12-23 11:59:56 -0800

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/Button/Button";
+
+const About = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          fill={true}
+          alt=""
+          className={styles.img}
+        />
+        <div className={styles.imgText}>
+          <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+          <h2 className={styles.imgDesc}>
+            Handcrafting award winning digital experiences
+          </h2>
+        </div>
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Who Are We?</h1>
+          <p className={styles.desc}>
+            We provides a variety of services focused on helping clients 
+            succeed in the online world. If you're asking what we should do 
+            about digital agencies in terms of their operations, role in the market, 
+            or how to interact with them.
+            <br />
+            <br />
+            We are made up of professionals with specialized knowledge in 
+            areas like digital marketing, web development, SEO, content 
+            creation, branding, and social media.
+          </p>
+        </div>
+        <div className={styles.item}>
+          <h1 className={styles.title}>What We Do?</h1>
+          <p className={styles.desc}>
+          We offer services such as digital marketing, web development, SEO, content creation, and brand strategy to meet the needs of businesses looking to improve their digital presence and achieve their business goals.
+            <br />
+            <br /> - Dynamic Websites
+            <br />
+            <br /> - Fast and Handy
+            <br />
+            <br /> - Mobile Apps
+          </p>
+          <Button url="/contact" text="Contact" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/app/portfolio/[category]/data.js
@@ -0,0 +1,71 @@
+export const items = {
+    applications: [
+      {
+        id: 1,
+        title: "Creative Portfolio",
+        desc: "Design and build websites tailored to a client’s needs, ensuring they are user-friendly, visually appealing, and optimized for performance.",
+        image:
+          "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+      },
+      {
+        id: 2,
+        title: "Minimal Single Product",
+        desc: "Develop and manage online stores, ensuring seamless shopping experiences.",
+        image:
+          "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+      },
+      {
+        id: 3,
+        title: "Strong Together Charity",
+        desc: "Build websites that clients can easily update and manage using platforms like WordPress, Drupal, or custom solutions.",
+        image:
+          "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+      }
+    ],
+    illustrations: [
+      {
+        id: 1,
+        title: "Creative Portfolio",
+        desc: "Improve a website’s ranking on search engines (like Google) through content optimization, technical fixes, and link-building strategies.",
+        image:
+          "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+      },
+      {
+        id: 2,
+        title: "Minimal Single Product",
+        desc: "Manage paid ad campaigns (such as Google Ads, Bing Ads, and social media ads) to drive traffic and conversions.",
+        image:
+          "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+      },
+      {
+        id: 3,
+        title: "Strong Together Charity",
+        desc: "Create and manage social media profiles, run advertising campaigns, and engage with audiences across platforms like Facebook, Instagram, LinkedIn, etc.",
+        image:
+          "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+      }
+    ],
+    websites: [
+      {
+        id: 1,
+        title: "Creative Portfolio",
+        desc: "Develop and execute email marketing campaigns to nurture leads, promote offers, or build customer loyalty.",
+        image:
+          "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+      },
+      {
+        id: 2,
+        title: "Minimal Single Product",
+        desc: "Partner with influencers to promote products/services and boost brand visibility.",
+        image:
+          "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+      },
+      {
+        id: 3,
+        title: "Strong Together Charity",
+        desc: "Create or refine a brand’s identity, including logo design, color schemes, and brand messaging.",
+        image:
+          "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+      }
+    ],
+  };
