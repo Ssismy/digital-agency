b7dff54269d642514866fdf5b407185b036b42ae
Ssismy <100096624+Ssismy@users.noreply.github.com>
2024-12-25 11:15:37 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
     images: {
       domains: ["images.pexels.com"],
     },
+    output: 'export',
+    typescript: {
+        ignoreBuildErrors: true,
+    }
   };
   
   export default nextConfig;
